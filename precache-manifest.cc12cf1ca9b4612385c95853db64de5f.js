c548217c5444cf436aca4857fe293dcfeb8e92ac
Chuah Songlin <chuahsonglin@Chuahs-MacBook-Pro.local>
2020-10-30 22:09:25 +0800

    Updates

---
+++ precache-manifest.cc12cf1ca9b4612385c95853db64de5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a48d5c48aec5ed9a7d5a73fde66f16",
+    "url": "/smw/index.html"
+  },
+  {
+    "revision": "2c047b5abf33300c7838",
+    "url": "/smw/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d1618aaaccddceaab92e",
+    "url": "/smw/static/css/main.d5ba6927.chunk.css"
+  },
+  {
+    "revision": "2c047b5abf33300c7838",
+    "url": "/smw/static/js/2.f5385bd1.chunk.js"
+  },
+  {
+    "revision": "9956f638ca73b88e6779ed946b7af8bb",
+    "url": "/smw/static/js/2.f5385bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1618aaaccddceaab92e",
+    "url": "/smw/static/js/main.1057e252.chunk.js"
+  },
+  {
+    "revision": "f85581586e59b94741cb",
+    "url": "/smw/static/js/runtime-main.fea2c970.js"
+  }
+]);
